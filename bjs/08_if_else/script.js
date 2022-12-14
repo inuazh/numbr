9d548fdaa199ba72691cf0dbeb9f4a6e799ccfaf
Ramazan <inuazh.work@yandex.ru>
2022-12-14 22:30:55 +0300

    1

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -37,6 +37,26 @@ document.getElementById('btnOver').addEventListener('click', function () {
     }
 })
 
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (maxValue === minValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
+})
+
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
         answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
@@ -44,3 +64,6 @@ document.getElementById('btnEqual').addEventListener('click', function () {
     }
 })
 
+function refreshPage(){
+    window.location.reload();
+} 
